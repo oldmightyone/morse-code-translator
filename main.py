625a826772ac460439a2128198db596dd043d413
Bish <pg3d224732156@gmail.com>
2023-02-23 15:13:45 -0500

    flask website setup completed

--- main.py
+++ main.py
@@ -8,14 +8,24 @@ def home():
     return redirect(url_for("text_to_morse"))
 
 
-@app.route("/text-morse")
+@app.route("/text-morse", methods=["GET", "POST"])
 def text_to_morse():
+    if request.method == "POST":
+        print(request.form.get("input"))
+        print(request.form.get("check"))
+        return redirect(url_for("morse_to_text"))
+
     return render_template("text-morse.html")
 
 
-@app.route("/morse-text")
+@app.route("/morse-text", methods=["GET", "POST"])
 def morse_to_text():
+    if request.method == "POST":
+        print(request.form.get("input"))
+        return redirect(url_for("text_to_morse"))
+
     return render_template("morse-text.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
